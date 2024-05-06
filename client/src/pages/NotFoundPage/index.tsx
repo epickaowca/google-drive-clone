a05e4feb3d827e286a7ec5362b12fac84b9df95e
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-05-06 09:09:43 +0200

    redirect user to home on NotFoundPage

--- client/src/pages/NotFoundPage/index.tsx
+++ client/src/pages/NotFoundPage/index.tsx
@@ -1,11 +1,6 @@
 import { FC } from "react";
-import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export const NotFoundPage: FC = () => {
-  return (
-    <div className="text-center position-fixed top-50 start-50 translate-middle">
-      <p>This page does not exist</p>
-      <Link to="/">Go back to home</Link>
-    </div>
-  );
+  return <Navigate to="/" />;
 };
